bb2c6d372f81d8c8e275a84f5d48b5104475465b
Jason Fernandes <18moneyj@gmail.com>
2021-09-22 14:50:57 -0500

    calc completed

---
+++ forms/calc/calc.js
@@ -0,0 +1,11 @@
+
+function calculate(num1, num2) {
+   let answer = num1 + num2
+   return answer
+}
+
+let num1 = prompt('Enter your first number:')
+let num2 = prompt('Enter your second number:')
+
+let summedNumbers = calculate(num1, num2)
+console.log(`The answer is ${summedNumbers}`)
